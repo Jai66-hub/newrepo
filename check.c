288f71d7eb470b46dee4f11dc6ec0ef5d050d468
Jai66-hub <79086172+Jai66-hub@users.noreply.github.com>
2023-02-25 17:55:02 +0530

    Update check.c

--- check.c
+++ check.c
@@ -19,15 +19,13 @@ typedef struct listing
 	int price;
 }Slist;
 
-Slist getfields(char *line);
+Slist	getfields(char *line);
+int		sortPrice(const void *PrevVal, const void *NxtVal);
+int		sortHostname(const void *PrevVal, const void *NxtVal);
+void	sortByPrice(Slist *list, int listCnt);
+void	sortByHostName(Slist *list, int listCnt);
+void	SortandWritedatatoFile(Slist *list, char *header, int listcnt, int SortOption /* 1-price sort, 2 - hostanme sort*/);
 
-int compare(const void *PrevVal, const void *NxtVal)
-{
-	const Slist *orderA = (Slist *)PrevVal;
-	const Slist *orderB = (Slist *)NxtVal;
-
-	return ((orderB->price) - (orderA->price));
-}
 
 int main()
 {
@@ -36,11 +34,9 @@ int main()
 	char	cpdata[1000];
 	int		cnt = 0;
 	FILE	*Ipfp = NULL;
-	FILE	*Opfp = NULL;
+	int		dataCnt = 0;
 
-
-//	Ipfp = fopen("C:/Users/DummuJay/source/repos/ConsoleApplication1/Debug/listings.csv", "r");
-	Ipfp = fopen("C:/Users/DummuJay/source/repos/ConsoleApplication1/ConsoleApplication1/listings.csv", "r");
+	Ipfp = fopen("listings.csv", "r");
 
 	if (Ipfp == NULL)
 	{
@@ -54,45 +50,58 @@ int main()
 	while (fgets(data, 1000, Ipfp) != NULL)
 		cnt++;
 
-	printf("Count value :%d\n", cnt);
-
 	list = (Slist*)malloc(sizeof(Slist)*cnt);
+
+	dataCnt = cnt;
 
 	fseek(Ipfp, 0, SEEK_SET);
 	cnt = 0;
 	fgets(data, 1000, Ipfp); // dummu read to escape header
 
-	while (fgets(data, 1000, Ipfp) != NULL)
+	while(fgets(data, 1000, Ipfp) != NULL)
 	{
 		list[cnt] = getfields(data);
 		cnt += 1;
 	}
 
-	printf("Count value :%d\n", cnt);
+	//sort by price 
+	SortandWritedatatoFile(list, cpdata, dataCnt, 1 /* 1-price sort, 2 - hostanme sort*/);
 
-	qsort(list, cnt+1, sizeof(Slist), compare);
+	// sort by hostname
+	SortandWritedatatoFile(list, cpdata, dataCnt, 2 /* 1-price sort, 2 - hostanme sort*/);
 
-	Opfp = fopen("sortedlist.csv", "w+");
+	free(list);
+	list = NULL;
+	fclose(Ipfp);
+	Ipfp = NULL;
+
+	return 0;
+}
+
+void SortandWritedatatoFile(Slist *list,char *header,int listcnt,int SortOption /* 1-price sort, 2 - hostanme sort*/)
+{
+	FILE *Opfp = NULL;
+
+	if(SortOption ==1)
+	{
+		sortByPrice(list, listcnt); // sort by price
+		Opfp = fopen("priceSortedlist.csv", "w+");
+	}
+	else
+	{
+		sortByHostName(list, listcnt); // sort by hostanme
+		Opfp = fopen("hostNameSortedlist.csv", "w+");
+	}
 
 	if (Opfp == NULL)
 	{
 		printf("Unable to create a file");
-		return 0;
+		return;
 	}
 
-	printf("Count value :%d", cnt);
+	fprintf(Opfp, "%s", header);
 
-	fprintf(Opfp, "%s", cpdata);
-
-/*	for (int loop = 0; loop < cnt; loop++)
-	{
-		printf("%d,%d,%s,%s,%s,%f,%f,%s,%d,%d,%d,%d,%d\n", list[loop].id, list[loop].host_id,
-			list[loop].hostname, list[loop].neighbourhood_group, list[loop].neighbourhood, list[loop].latitude,
-			list[loop].longitude, list[loop].room_type, list[loop].price, list[loop].minimum_nights, list[loop].number_of_reviews,
-			list[loop].calculated_host_listings_count, list[loop].availability_365);
-	}*/
-
-	for (int loop = 0; loop < cnt; loop++)
+	for (int loop = 0; loop < listcnt; loop++)
 	{
 		fprintf(Opfp, "%d,%d,%s,%s,%s,%f,%f,%s,%d,%d,%d,%d,%d\n", list[loop].id, list[loop].host_id,
 			list[loop].hostname, list[loop].neighbourhood_group, list[loop].neighbourhood, list[loop].latitude,
@@ -100,12 +109,34 @@ int main()
 			list[loop].calculated_host_listings_count, list[loop].availability_365);
 	}
 
-	free(list);
-	list = NULL;
-	fclose(Ipfp);
-	Ipfp = NULL;
 	fclose(Opfp);
 	Opfp = NULL;
+
+	return;
+}
+
+void sortByPrice(Slist *list, int listCnt)
+{
+	qsort(list, listCnt, sizeof(Slist), sortPrice);
+}
+
+int sortPrice(const void *PrevVal, const void *NxtVal)
+{
+	const Slist *list1 = (Slist *)PrevVal;
+	const Slist *list2 = (Slist *)NxtVal;
+	return ((list2->price) - (list1->price));
+}
+
+void sortByHostName(Slist *list, int listCnt)
+{
+	qsort(list, listCnt, sizeof(Slist), sortHostname);
+}
+
+int sortHostname(const void *PrevHoststr, const void *NextHostStr)
+{
+	const Slist *list1 = (Slist *)PrevHoststr;
+	const Slist *list2 = (Slist *)NextHostStr;
+	return strcmp(list1->hostname, list2->hostname);
 }
 
 Slist getfields(char *line)
