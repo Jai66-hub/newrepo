6e0a1a344edf5786aefec96ab0cbbb9ef6b8b45a
Jai66-hub <79086172+Jai66-hub@users.noreply.github.com>
2023-03-08 22:04:24 +0530

    Update check.c

--- check.c
+++ check.c
@@ -6,18 +6,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+
 int main(int argc, char **argv)
 {
-	
-	pid_t	pid;
-	int		status = 0;
-	char	data[1000];
-	char	command[1000];
-	char	TimestampBefore[250];
-	char	TimestampAfter[250];
+
+	pid_t	ChildPid;
+	int	ChildStatus = 0;
+	char	data[250] = { 0 };
+	char	command[250] = { 0 };
 	FILE	*Ipfp = NULL;
 	FILE	*OPfp = NULL;
-	time_t	begin, end;
+	time_t	beginTime, endTime;
+	char	argumnets[10][250] = { 0 };
+	char	*args = NULL;
+	int	argcnt = 0;
 
 	Ipfp = fopen(argv[1], "r");
 
@@ -32,37 +34,48 @@ int main(int argc, char **argv)
 	if (OPfp == NULL)
 	{
 		printf("Unable to create a log file\n");
+		fclose(Ipfp);
+		Ipfp = NULL;
 		return 0;
 	}
 
-	while (fgets(data, 1000, Ipfp) != NULL)
+	while (fgets(data, 250, Ipfp) != NULL)
 	{
 		strcpy(command, data);
+		argcnt = 0;
+		memset(argumnets, 0, sizeof argumnets);
+		args = strtok(command, " ");
+		strcpy(argumnets[argcnt], args);
+		while (args != NULL)
+		{
+			args = strtok(NULL, " ");
+			argcnt++;
+			if(args != NULL)
+				strcpy(argumnets[argcnt], args);
+		}
 
-		begin = time(NULL);
-		pid = fork();
+		beginTime = time(NULL);
+		ChildPid = fork();
 
-		if (pid == 0)
+		if (ChildPid == 0)
 		{
 			printf("this is child\n");
-			execlp(command, command, (char *)NULL);
+			execvp(argumnets[0], argumnets);
 			exit(0);
 		}
-		else if (pid > 0)
+		else if (ChildPid > 0)
 		{
 			printf("this is parent\n");
-			waitpid(pid,&status);
-			end = time(NULL);
-			if (WIFEXITED(status))
+			waitpid(ChildPid, &ChildStatus);
+			endTime = time(NULL);
+			if (WIFEXITED(ChildStatus))
 			{
 				printf("child exited normally");
 			}
-			else {
-				printf("child process not terminated normally");
-			}
-			status = 0;
 
-			fprintf(OPfp,"%s\t%s\t%s\n", command, ctime(&begin), ctime(&end));
+			ChildStatus = 0;
+
+			fprintf(OPfp, "%s\t%s\t%s\n", command, ctime(&beginTime), ctime(&endTime));
 		}
 		else
 		{
